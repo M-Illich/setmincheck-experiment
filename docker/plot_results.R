6a9cabcaa33d97477a1b9d7e97b264990246ec78
M-Illich <moritz.illich@uni-ulm.de>
2020-04-29 14:55:47 +0200

    adding plot for summary of results, i.e., mean of measured times, and altering of maven jar creation to store jar-file in docker folder

--- docker/plot_results.R
+++ docker/plot_results.R
@@ -2,6 +2,17 @@
 results <- read.csv("results.csv")
 # number of tested set representation objects
 objectNum <- ncol(results) - 1
+
+# get mean of measured times for each tested object
+sumObj <- data.frame(matrix(nrow=1,ncol=ncol(results)))
+sumObj[1] <- "summary"
+for (i in 2:ncol(sumObj)) {
+  sumObj[i] <- mean(results[,i])
+}
+# add to results
+colnames(sumObj) <- names(results)
+results <- rbind(results,data.frame(sumObj))
+
 # number of tested files
 fileNum <- nrow(results)
 # names of tested files
@@ -10,7 +21,7 @@ fileNames <- results[, 1]
 # plot results
 for (i in 1:fileNum) {
   # create pdf for plot
-  pdf(paste0("plots/plot_", fileNames[i], ".pdf"))
+#  pdf(paste0("plots/plot_", fileNames[i], ".pdf"))
   barplot(
     unlist(results[i, 2:(objectNum + 1)]),
     xlab = "test objects",
