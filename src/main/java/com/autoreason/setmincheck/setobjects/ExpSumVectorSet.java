1e728c25b3b2804590dbf3e8d72bf9c51e83cfd1
M-Illich <moritz.illich@uni-ulm.de>
2020-05-19 16:14:08 +0200

    integration of SumVectorSet implementation

---
+++ src/main/java/com/autoreason/setmincheck/setobjects/ExpSumVectorSet.java
@@ -0,0 +1,45 @@
+package com.autoreason.setmincheck.setobjects;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.autoreason.setmincheck.AbSetRepSubsetChecker;
+import com.autoreason.setmincheck.MatchProvider;
+import com.autoreason.setmincheck.SubsetChecker;
+
+/**
+ * An {@link ExpSetRepresent} implementation for {@link SumVectorSet}
+ *
+ */
+public class ExpSumVectorSet implements ExpSetRepresent<SumVectorSet> {
+
+	@Override
+	public MatchProvider<SumVectorSet, Set<?>> getMatchProvider() {
+		return new SumVecSetMatchProvider();
+	}
+
+	@Override
+	public SubsetChecker<SumVectorSet> getSubsetChecker() {
+		return new AbSetRepSubsetChecker<SumVectorSet>();
+	}
+
+	@Override
+	public Collection<SumVectorSet> convertCollection(Collection<Set<Integer>> col) {
+		// new collection for converted sets
+		Collection<SumVectorSet> convertedCol = new HashSet<>();
+		// go through each element of col
+		for (Set<?> set : col) {
+			// add converted set to new collection
+			convertedCol.add(new SumVectorSet(set));
+		}
+
+		return convertedCol;
+	}
+	
+	@Override
+	public SumVectorSet getSetRepresent(Set<?> set) {
+		return new SumVectorSet(set);
+	}
+
+}
