de4f96bea7faea13b29bdaf38a1ddc394795cdaf
M-Illich <moritz.illich@uni-ulm.de>
2020-05-19 16:15:41 +0200

    adapted plot

--- docker/plot_results.R
+++ docker/plot_results.R
@@ -3,30 +3,21 @@ results <- read.csv("results.csv")
 # number of tested set representation objects
 objectNum <- ncol(results) - 1
 
-# get mean of measured times for each tested object
-#sumObj <- data.frame(matrix(nrow=1,ncol=ncol(results)))
-#sumObj[1] <- "summary"
-#for (i in 2:ncol(sumObj)) {
-#  sumObj[i] <- mean(results[,i])
-#}
-## add to results
-#colnames(sumObj) <- names(results)
-#results <- rbind(results,data.frame(sumObj))
-
 # number of tested files
 fileNum <- nrow(results)
 # names of tested files
 fileNames <- results[, 1]
 
-# show results in relation to first entry 
+# show results in relation to first entry
 for (i in 1:fileNum) {
-  results[i,2:ncol(results)] <- results[i,2:ncol(results)] / results[i,2]
+  results[i, 2:ncol(results)] <-
+    results[i, 2:ncol(results)] / results[i, 2]
 }
 
 # plot results
 for (i in 1:fileNum) {
   # create pdf for plot
-  pdf(paste0("plots/plot_", fileNames[i], ".pdf"))
+  pdf(paste0("plots/plot_", fileNames[i], ".pdf"), width = 10)
   barplot(
     unlist(results[i, 2:(objectNum + 1)]),
     xlab = "test objects",
@@ -34,7 +25,8 @@ for (i in 1:fileNum) {
     main = fileNames[i],
     col = grey.colors(objectNum),
     axes = TRUE,
-    axis.lty = 1
+    axis.lty = 1,
+    las = 1
   )
 }
 
