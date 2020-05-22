02ff4f7bfbbc74a1750301db38aed84732bc7f3c
M-Illich <moritz.illich@uni-ulm.de>
2020-05-22 14:59:34 +0200

    adapted plot to show absolute times in milliseconds

--- docker/plot_results.R
+++ docker/plot_results.R
@@ -9,21 +9,21 @@ fileNum <- nrow(results)
 fileNames <- results[, 1]
 
 # show results in relation to first entry
-for (i in 1:fileNum) {
-  results[i, 2:ncol(results)] <-
-    results[i, 2:ncol(results)] / results[i, 2]
-}
+#for (i in 1:fileNum) {
+#  results[i, 2:ncol(results)] <-
+#    results[i, 2:ncol(results)] / results[i, 2]
+#}
 
 # plot results
 for (i in 1:fileNum) {
   # create pdf for plot
   pdf(paste0("plots/plot_", fileNames[i], ".pdf"), width = 10)
-  y <- unlist(results[i, 2:(objectNum + 1)])
+  y <- floor(unlist(results[i, 2:(objectNum + 1)]) / 1000)
   x <- barplot(
     y,
     ylim = c(0,max(y)*1.1),
     xlab = "test objects",
-    ylab = "relative execution time",
+    ylab = "execution time [ms]",
     main = fileNames[i],
     col = grey.colors(objectNum),
     axes = TRUE,
